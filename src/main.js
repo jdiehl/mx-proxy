f6de79c74e474eaaa4a029c9ba81527857c454e2
Jonathan Diehl <jonathan.diehl@gmail.com>
2023-03-27 12:22:35 +0200

    added https

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ const { logger } = require('./logger')
 const { proxy } = require('./proxy')
 
 exports.main = async () => {
-  const server = proxy('http://10.211.55.15:8080', logger)
-  server.listen(8080)
+  const server = await proxy('http://10.211.55.15:8080', logger)
+  server.listen(443)
 }
--- src/proxy.js
+++ src/proxy.js
@@ -1,11 +1,28 @@
-const http = require('http')
+const https = require('https')
 const httpProxy = require('http-proxy')
+const pem = require('pem')
    
-exports.proxy = (target, onRequest) => {
+const certProps = {
+  days: 1, // Validity in days
+  selfSigned: true,
+};
+
+async function createCredentials() {
+  return new Promise((resolve, reject) => {
+    pem.createCertificate(certProps, (error, keys) => {
+      if (error) return reject(error)
+      resolve({ key: keys.serviceKey, cert: keys.certificate })
+    });
+  })
+}
+
+exports.proxy = async (target, onRequest) => {
   const proxy = httpProxy.createProxyServer()
   proxy.on('proxyRes', onRequest)
 
-  const server = http.createServer(function(req, res) {
+  const credentials = await createCredentials()
+
+  const server = https.createServer(credentials, (req, res) => {
     proxy.web(req, res, { target })
   })
 
